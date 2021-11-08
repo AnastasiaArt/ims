123f2e8903315ee29ff1bdffb8000a6b3b3f297a
art <art-nastya@inbox.ru>
2021-11-09 00:55:04 +0300

    added email, fixed title

--- dist/js/services.js
+++
@@ -1,1 +0,0 @@
-const services=document.querySelectorAll(".services__item--second");function setDelayInServices(e){services.forEach((s=>{s.setAttribute("data-wow-delay",e)}))}function changeDelay(){window.innerWidth>1040?setDelayInServices("0.8s"):setDelayInServices("0.4s")}module.exports=changeDelay;
